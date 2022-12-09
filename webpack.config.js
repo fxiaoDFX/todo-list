c62db2cb96f4fb44401ba34a34c218624be1e1e7
Frankie Xiao <fxiaoX089@live.com>
2022-12-09 13:51:41 -0600

    setup webpack devServer

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "source-map",
+    devServer: {
+        static: "./dist",
+    },
+    optimization: {
+        runtimeChunk: "single",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Todo List",
+            filename: "index.html",
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+}

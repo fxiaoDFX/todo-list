e31cbe32dc57af66dfadfb70ae2f367d4059978b
Frankie Xiao <fxiaoX089@live.com>
2022-12-17 17:31:17 -0600

    add html for static elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import "./style.css"
 import createProject from "./modules/projectFactoryFunction"
-import { DOM } from "./modules/DOMLoaderFunction"
 import controller from "./modules/controller"
 
-console.log(createProject("Snacks"))
+//console.log(createProject("Snacks"))
 
-DOM.loadContentStartup()
-
-controller
+controller.render()
--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,6 +1,23 @@
 const controller = (() => {
     const listContainer = document.querySelector("[data-list]")
-    let list = []
+    let list = ["grocery", "things"]
+
+    function render() {
+        clearContainer(listContainer)
+        list.forEach((item) => {
+            const listElement = document.createElement("li")
+            listElement.classList.add("listName")
+            listElement.innerText = item
+            listContainer.append(listElement)
+        })
+    }
+    return { render }
 })()
+
+function clearContainer(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
 
 export default controller

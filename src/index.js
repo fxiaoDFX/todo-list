71d3e4350e342123e3172497b17175c70dc6689e
Frankie Xiao <fxiaoX089@live.com>
2022-12-18 17:34:56 -0600

    add event listener for delete buttons

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import "./style.css"
 import createProject from "./modules/projectFactoryFunction"
 import controller from "./modules/controller"
 
-//console.log(createProject("Snacks"))
-
 controller.render()
--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,23 +1,83 @@
+import createProject from "./projectFactoryFunction"
+
 const controller = (() => {
     const listContainer = document.querySelector("[data-list]")
-    let list = ["grocery", "things"]
+    const newProjectForm = document.querySelector("[data-new-project-form]")
+    const newProjectInput = document.querySelector("[data-new-project-input]")
+    const deleteProjectButton = document.querySelector(
+        "[data-delete-project-button]"
+    )
+
+    // Local Storage Keys
+    const LOCAL_STORAGE_LIST_KEY = "project.list"
+    const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "project.selectedListId"
+
+    // value that will be used in local storage
+    let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [
+        createProject("Default"),
+    ]
+    let selectedListId = localStorage.getItem(
+        LOCAL_STORAGE_SELECTED_LIST_ID_KEY
+    )
+
+    // event listeners
+    newProjectForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const projectName = newProjectInput.value
+        if (projectName === null || projectName === "") return
+        const newList = createProject(projectName)
+        lists.push(newList)
+        newProjectInput.value = null
+        saveAndRender()
+    })
+
+    listContainer.addEventListener("click", (e) => {
+        if (e.target.tagName.toLowerCase() === "li") {
+            if (e.target.classList.contains("active-list")) {
+                e.target.classList.remove("active-list")
+                selectedListId = null
+            } else selectedListId = e.target.dataset.listId
+            saveAndRender()
+        }
+    })
+
+    deleteProjectButton.addEventListener("click", () => {
+        lists = lists.filter((project) => project.id !== selectedListId)
+        saveAndRender()
+    })
+
+    // functions
+    function saveAndRender() {
+        save()
+        render()
+    }
+
+    function save() {
+        localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+        localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+    }
 
     function render() {
         clearContainer(listContainer)
-        list.forEach((item) => {
+        lists.forEach((project) => {
             const listElement = document.createElement("li")
+            listElement.dataset.listId = project.id
             listElement.classList.add("listName")
-            listElement.innerText = item
+            listElement.innerText = project.title
+            if (project.id === selectedListId) {
+                listElement.classList.add("active-list")
+            }
             listContainer.append(listElement)
         })
     }
+
+    function clearContainer(element) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild)
+        }
+    }
+
     return { render }
 })()
-
-function clearContainer(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
-}
 
 export default controller
